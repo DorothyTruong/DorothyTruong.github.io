1a74b51cc38fbc112922ad76cb0d2affc54fb736
DorothyTruong <truongdorothy@hotmail.com>
2019-09-27 00:03:37 -0700

    toggle form

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+// function myFunction() {
+//     var body = document.getElementsByTagName('body')[0]; 
+//     var btn = document.createElement("BUTTON");
+//     body.appendChild(btn);
+// }
+
 function myFunction() {
-    var body = document.getElementsByTagName('body')[0]; 
-    body.appendChild(btn);
-    console.log(btn);
-    console.log(body);
-}
+    var x = document.getElementById("add-form");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
