7df72c87c96165ef3346926722c9f91f19f3d018
DorothyTruong <truongdorothy@hotmail.com>
2019-09-27 11:43:00 -0700

    button-add users

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// function myFunction() {
-//     var body = document.getElementsByTagName('body')[0]; 
-//     var btn = document.createElement("BUTTON");
-//     body.appendChild(btn);
-// }
-
 function myFunction() {
     var x = document.getElementById("add-form");
     if (x.style.display === "none") {
@@ -11,4 +5,29 @@ function myFunction() {
     } else {
       x.style.display = "none";
     }
-  }
+}
+
+
+function myInput(){
+    var input = document.querySelector("#add-form");  //this accesses my #add-form in my html
+    var allArtists = document.querySelector('#all-artists');          //this is a way to access my div in my html
+    var containerDiv = document.createElement('div');
+    var image = document.createElement('img');
+    var infoDiv = document.createElement('div');
+    infoDiv.classList.add('info');  // way of adding a class to my infoDiv
+    var p1 = document.createElement('p');
+    var header = document.createElement('h2');
+    var name = document.createTextNode(input.elements[0].value);
+    var description = document.createTextNode(input.elements[1].value);
+    var p2 = document.createElement('p');
+    allArtists.appendChild(containerDiv);                //appending my new div to my div in my html
+    containerDiv.classList.add('container');
+    image.setAttribute('src', input.elements[2].value);
+    containerDiv.appendChild(image);
+    containerDiv.appendChild(infoDiv);
+    infoDiv.appendChild(p1);
+    p1.appendChild(header);
+    header.appendChild(name);
+    infoDiv.appendChild(p2);
+    p2.appendChild(description); 
+}
